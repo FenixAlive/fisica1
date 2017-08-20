8084d4857283ca245f45e21f5843045fe241e684
FenixAlive <luisangelmf@gmail.com>
2017-08-19 19:11:33 -0500

    terminado mrua

--- views/u1/js/mrua.js
+++ views/u1/js/mrua.js
@@ -1,12 +1,44 @@
 $(function(){
-	$("#bd").click(function(e){
+	$("#bv").click(function(e){
 		e.preventDefault();
-		var dt=$("#dt").val();
-		var dv=$("#dv").val();
-		var rd=dt*dv;
-		$("#rd").empty();
-		$("#rd").append('<div class="card-panel small teal darken-3 res">'+rd+'</div>')
-
+		var vt=$("#vt").val();
+		var va=$("#va").val();
+		var vi=$("#vi").val();
+		var vf=$("#vf").val();
+		if(vf){
+			var rv=vf-va*vt;
+		$("#rv").empty();
+		$("#rv").append('<div class="card-panel small red darken-3 res">Velocidad Inicial: '+rv+'</div>')
+		}else if(vi){
+			console.log("vi")
+			var rv=vi+va*vt;
+		$("#rv").empty();
+		$("#rv").append('<div class="card-panel small red darken-3 res">Velocidad Final: '+rv+'</div>')
+		}else{
+			console.log("sin velocidades")
+		$("#rv").empty();
+		$("#rv").append('<div class="card-panel small red darken-3 res">Es necesario llenar una velocidad.</div>')
+		}		
+		$(":text").each(function(){	
+			$($(this)).val('');
+		});
+	});
+	$("#ba").click(function(e){
+		e.preventDefault();
+		var at=$("#at").val();
+		var ad=$("#ad").val();
+		var avi=$("#avi").val();
+		var avf=$("#avf").val();
+		var ra="Es necesario llenar los campos solicitados";
+		if(avi && avf){
+			ra=(avf-avi)/at;
+		}else if(at && ad && !avf){
+			ra=(2*ad)/(at*at);
+		}else{
+			ra="Es necesario llenar los campos solicitados";
+		}
+		$("#ra").empty();
+		$("#ra").append('<div class="card-panel small red darken-3 res">'+ra+'</div>');
 		$(":text").each(function(){	
 			$($(this)).val('');
 		});
@@ -14,21 +46,38 @@ $(function(){
 	$("#bt").click(function(e){
 		e.preventDefault();
 		var td=$("#td").val();
-		var tv=$("#tv").val();
-		var rt=td/tv;
+		var ta=$("#ta").val();
+		var tvi=$("#tvi").val();
+		var tvf=$("#tvf").val();
+		var rt="Es necesario llenar los campos solicitados";
+		if(tvi && tvf){
+			rt=(tvf-tvi)/ta;
+		}else if(td && ta && !tvf){
+			rt=Math.sqrt((2*td)/ta);
+		}else{
+			rt="Es necesario llenar los campos solicitados";
+		}
 		$("#rt").empty();
-		$("#rt").append('<div class="card-panel small teal darken-3 res">'+rt+'</div>');
+		$("#rt").append('<div class="card-panel small red darken-3 res">'+rt+'</div>');
 		$(":text").each(function(){	
 			$($(this)).val('');
 		});
 	});
-	$("#bv").click(function(e){
+	$("#bd").click(function(e){
 		e.preventDefault();
-		var vd=$("#vd").val();
-		var vt=$("#vt").val();
-		var rv=vd/vt;
-		$("#rv").empty();
-		$("#rv").append('<div class="card-panel small teal darken-3 res">'+rv+'</div>');
+		var dt=$("#dt").val();
+		var dvi=$("#dvi").val();
+		var da=$("#da").val();
+		var rd="Solo se permite dejar en blanco la velocidad inicial en caso de ser 0, los demas campos son obligatorios";
+		if(dvi && dvi != 0){
+			rd=(dvi*dt)+(0.5*da*(dt*dt));
+		}else if(dt && da){
+			rd=0.5*da*(dt*dt);
+		}else{
+			rd="Solo se permite dejar en blanco la velocidad inicial en caso de ser 0, los demas campos son obligatorios";
+		}
+		$("#rd").empty();
+		$("#rd").append('<div class="card-panel small red darken-3 res">'+rd+'</div>');
 		$(":text").each(function(){	
 			$($(this)).val('');
 		});
